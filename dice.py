10e09160ba4c4bc94b309bb906ef0d12ec8961cb
solargravel <astridneb@gmail.com>
2025-07-08 18:47:35 -0300

    Pylance changes and added docstrings.

--- dice.py
+++ dice.py
@@ -1,28 +1,36 @@
+"""Dice functionality and Classes.
+
+Example:
+    foo = roll_duality(...)
+"""
+
 from random import randint
 from enum import Enum, auto
-from typing import Optional
 from dataclasses import dataclass
 
-
 class RollType(Enum):
+    """Type of roll (Normal, Advantage, Disadvantage)."""
     NORMAL = auto()
     ADVANTAGE = auto()
     DISADVANTAGE = auto()
 
 
 class ModifierDie(Enum):
+    """Advantage/Disadvantage die type."""
     D6 = auto()
     D8 = auto()
     D10 = auto()
 
 
 class HopeDie(Enum):
+    """Type of the hope die to be rolled."""
     D12 = auto()
     D20 = auto()
 
 
 @dataclass
 class RollData:
+    """Dataclass of a roll and all of it's fields."""
     base_modifier: int
     roll_type: RollType
     hope_die: HopeDie
@@ -30,50 +38,68 @@ class RollData:
     amount: int
 
 
-def roll_duality(base_modifier: int, roll_type: RollType, hope_die: HopeDie, modifier_die: ModifierDie) -> str:    
+def roll_duality(base_modifier: int, roll_type: RollType, hope_die: HopeDie,
+                 modifier_die: ModifierDie) -> str:
+    """Roll duality dice and returns a result string.
+
+    Args:
+        base_modifier (int): Modifier for the roll.
+        roll_type (RollType): Type of the roll. (Normal, Advantage, Disadvantage)
+        hope_die (HopeDie): Value of the hope die.
+        modifier_die (ModifierDie): Value of the Advantage/Disadvantage die.
+
+    Raises:
+        ValueError: _description_
+        ValueError: _description_
+        ValueError: _description_
+
+    Returns:
+        str: The result of the roll to be printed.
+    """
+
     fear_roll: int = randint(1, 12)
 
     hope_die_value: int
     match hope_die:
         case HopeDie.D12:
             hope_die_value = 12
-            
+
         case HopeDie.D20:
             hope_die_value = 20
-            
+
         case _:
             raise ValueError("Hope dice type doesn't exist.")
-    
+
     hope_roll: int = randint(1, hope_die_value)
-    
+
     modifier_die_type: int
     match modifier_die:
         case ModifierDie.D6:
             modifier_die_type = 6
-            
+
         case ModifierDie.D8:
             modifier_die_type = 8
-            
+
         case ModifierDie.D10:
             modifier_die_type = 10
-        
+
         case _:
             raise ValueError("Modifier dice type doesn't exist.")
-    
+
     modifier_type: int
     match roll_type:
         case RollType.ADVANTAGE:
             modifier_type = randint(1, modifier_die_type)
-            
+
         case RollType.DISADVANTAGE:
             modifier_type = -randint(1, modifier_die_type)
-            
+
         case RollType.NORMAL:
             modifier_type = 0
-        
+
         case _:
             raise ValueError("Roll type doesn't exist.")
-            
+
     roll_aligment: str
     if hope_roll == fear_roll:
         return (f"`Rolagem Crítica!` (**{hope_roll + fear_roll}**) ⟵ [**{hope_roll}**] Esperança "
@@ -82,32 +108,41 @@ def roll_duality(base_modifier: int, roll_type: RollType, hope_die: HopeDie, mod
         roll_aligment = "Esperança"
     else:
         roll_aligment = "Medo"
-    
+
     roll_value: int = fear_roll + hope_roll + modifier_type + base_modifier
-    roll_text: str = (f"`{roll_value} com {roll_aligment}` ⟵ [**{hope_roll}**] Esperança (d{hope_die_value}) + "
+    roll_text: str = (f"`{roll_value} com {roll_aligment}` "
+                      f"⟵ [**{hope_roll}**] Esperança (d{hope_die_value}) + "
                       f"[**{fear_roll}**] Medo (d12)")
-    
+
     if modifier_type > 0:
         roll_text += f" + [**{modifier_type}**] Vantagem (d{modifier_die_type})"
     elif modifier_type < 0:
         roll_text += f" - [**{abs(modifier_type)}**] Desvantagem (d{modifier_die_type})"
-    
+
     if base_modifier > 0:
         roll_text += f" + **{base_modifier}**"
     elif base_modifier < 0:
         roll_text += f" - **{abs(base_modifier)}**"
-    
+
     return roll_text
 
 
-def message_dict(message: str) -> Optional[RollData]:
+def roll_from_message(message: str) -> RollData | None:
+    """Generates RollData based on a message. RollData should then be used in roll_dice()
+
+    Args:
+        message (str): The message to be turned into RollData
+
+    Returns:
+        RollData | None: The RollData if it could be generated
+    """
     modifier: int = 0
     hope_die: HopeDie
     roll_type: RollType = RollType.NORMAL
     modifier_die: ModifierDie = ModifierDie.D6
-    
+
     raw_message = message.lower().strip().replace(" ", "")
-    
+
     if raw_message == "dd":
         # return roll_duality(0, RollTypes.NORMAL.value, HopeDice.D12.value, ModifierDice.D6.value)
         return RollData(
@@ -117,37 +152,37 @@ def message_dict(message: str) -> Optional[RollData]:
             modifier_die=ModifierDie.D6,
             amount=1,
         )
-    
+
     sliced_message: list[str] = raw_message.split("#")
-    
+
     roll_number: int
     if sliced_message[0].isdecimal():
         roll_number = int(sliced_message[0].split("#")[0])
     else:
         roll_number = 1
-    
-    if len(sliced_message) != 1:  
+
+    if len(sliced_message) != 1:
         raw_message = sliced_message[1]
-    
+
     if raw_message[0:2] != "dd":
         return None
-    
+
     match raw_message[2:4]:
         case "12":
             hope_die = HopeDie.D12
-        
+
         case "20":
             hope_die = HopeDie.D20
-            
+
         case _:
             hope_die = HopeDie.D12
-    
+
     vantage_num: int = 0
     for i, char in enumerate(raw_message[2:]):
         match char:
             case 'v':
                 vantage_num += 1
-                
+
                 match vantage_num:
                     case 1:
                         roll_type = RollType.ADVANTAGE
@@ -155,10 +190,10 @@ def message_dict(message: str) -> Optional[RollData]:
                         modifier_die = ModifierDie.D8
                     case _:
                         modifier_die = ModifierDie.D10
-            
+
             case 'i':
                 vantage_num += 1
-                
+
                 match vantage_num:
                     case 1:
                         roll_type = RollType.DISADVANTAGE
@@ -166,16 +201,16 @@ def message_dict(message: str) -> Optional[RollData]:
                         modifier_die = ModifierDie.D8
                     case _:
                         modifier_die = ModifierDie.D10
-            
+
             case '+' | '-':
                 try:
                     modifier: int = int(raw_message[i+2:])
                 except ValueError:
                     return None
-            
+
             case _:
                 pass
-    
+
     # return roll_multiple(modifier, roll_type, hope_die, modifier_die, roll_number)
     return RollData(
         base_modifier=modifier,
@@ -186,7 +221,15 @@ def message_dict(message: str) -> Optional[RollData]:
     )
 
 
-def roll_dice(roll: RollData) -> str:
+def roll_multiple(roll: RollData) -> str:
+    """Rolls duality based on RollData, can support mutiple rolls.
+
+    Args:
+        roll (RollData): The roll arguments data.
+
+    Returns:
+        str: The result of the roll to be printed.
+    """
     roll_message: str = ""
     
     if roll.amount == 1:
