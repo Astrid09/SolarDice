bedd9eee7cb41543fab8a76aed625362d41aaae8
solargravel <astridneb@gmail.com>
2025-07-08 19:49:01 -0300

    Implement message read functionality on bot for dice rolling.

--- bot.py
+++ bot.py
@@ -1,6 +1,26 @@
 """Main Bot class."""
 import discord
+from discord import Message
+import dice
 
 class Bot(discord.Bot):
-    async def on_ready(self):
+    """The discord Bot subclass.
+    """
+    async def on_ready(self) -> None:
+        """The event that runs when the bot gets ready.
+        """
         print(f"{self.user} is ready!")
+
+    async def on_message(self, message: Message) -> None:
+        """The event thar runs when a message is sent.
+
+        Args:
+            message (Message): The Message object.
+        """
+        if message.author == self.user:
+            return
+
+        if roll_result := dice.get_roll_from_message(message.content):
+            await message.reply(roll_result)
+
+        print(f"{message.author}: {message.content}")
--- main.py
+++ main.py
@@ -2,14 +2,16 @@
 
 import os
 from dotenv import load_dotenv
-from discord import ApplicationContext, guild_only, default_permissions
-from bot import Bot
+from discord import ApplicationContext, guild_only, default_permissions, Intents
+import bot
 
 load_dotenv()
 DISCORD_KEY: str | None = os.environ.get("BOT_TOKEN")
 TEST_GUILD: str | None = os.environ.get("TEST_GUILD")
 
-bot = Bot()
+intents: Intents = Intents.default()
+intents.message_content = True
+bot = bot.Bot(intents=intents)
 
 @bot.slash_command(
     name="test",
