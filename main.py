61127a677423ac60518ee7e0f6d1b5ef8ddd430c
solargravel <astridneb@gmail.com>
2025-07-08 18:25:22 -0300

    Made test function work only on testing server with admin.

--- main.py
+++ main.py
@@ -2,18 +2,22 @@
 
 import os
 from dotenv import load_dotenv
-from discord import ApplicationContext
+from discord import ApplicationContext, guild_only, default_permissions
 from bot import Bot
 
 load_dotenv()
 DISCORD_KEY: str | None = os.environ.get("BOT_TOKEN")
+TEST_GUILD: str | None = os.environ.get("TEST_GUILD")
 
 bot = Bot()
 
 @bot.slash_command(
     name="test",
-    description="Returns a \"Hello World\" for testing purpose."
+    description="Returns a \"Hello World\" for testing purpose.",
+    guild_id=[TEST_GUILD]
 )
+@default_permissions(administrator=True)
+@guild_only()
 async def hello_world(ctx: ApplicationContext):
     await ctx.respond("Hello World!")
 
